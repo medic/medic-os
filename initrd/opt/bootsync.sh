38c943beea3e9624f1d9e8d1ad1bebb18258140f
David Brown <github@scri.pt>
2012-10-31 12:21:02 -0700

    Initial commit.

---
+++ initrd/opt/bootsync.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# put other system startup commands here, the boot process will wait until they complete.
+# Use bootlocal.sh for system startup commands that can run in the background 
+# and therefore not slow down the boot process.
+
+/usr/bin/sethostname toaster &&
+exec /boot/system-start
+
