817e988ba28a4a0a84049661b94af5147f921fd1
David Brown <dave@scri.pt>
2013-01-22 11:27:21 -0800

    Refactor of concierge -- the vm-toaster first-run app.

--- packages/concierge/software/concierge/v1.0.0/server.js
+++ packages/concierge/software/concierge/v1.0.0/server.js
@@ -3,6 +3,8 @@ var child = require('child_process'),
     flash = require('connect-flash'),
     request = require('request'),
     express = require('express'),
+    crypto = require('crypto'),
+    async = require('async'),
     fs = require('fs'),
     app = express();
 
@@ -63,9 +65,9 @@ app.post('/setup/password', function (req, res) {
     req.flash('key', key);
   }
 
-  set_system_password(req, password, confirmation, function (_was_set) {
+  set_password(req, password, confirmation, function (_err) {
 
-    if (!_was_set) {
+    if (_err) {
       return res.redirect('/setup');
     }
 
@@ -89,6 +91,7 @@ app.post('/setup/password', function (req, res) {
 });
 
 /**
+ * fatal:
  */
 var fatal = function (_message, _exception) {
 
@@ -97,6 +100,7 @@ var fatal = function (_message, _exception) {
 };
 
 /**
+ * trim:
  */
 var trim = function (_string) {
 
@@ -106,8 +110,47 @@ var trim = function (_string) {
   );
 };
 
+/**
+ * request_error:
+ *  Add `_message` to `_req.flash`, invoke `_cb` with a new
+ *  error object, and then return whatever that callback returned.
+ */
+
+var request_error = function (_message, _req, _callback) {
+
+  _req.flash('error', _message);
+
+  return _callback(
+    new Error(_message)
+  );
+};
+
 
 /**
+ * check_response:
+ *  Check both `_err` and `_resp.statusCode`, then fill
+ *  `_req.flash` if necessary and return a single `_err` object.
+ */
+
+var check_response = function (_err, _resp, _req, _text, _cb) {
+
+  if (_err) {
+    return _cb(_err);
+  }
+
+  if (_resp.statusCode !== 200) {
+    return request_error(
+      _text + ' failed: ' + 'problem with database server',
+        _req, _cb
+    );
+  }
+
+  return _cb();
+};
+
+
+/**
+ * add_openssh_public_key:
  */
 var add_openssh_public_key = function (_req, _key, _callback) {
 
@@ -125,18 +168,22 @@ var add_openssh_public_key = function (_req, _key, _callback) {
   passwd.on('exit', function (_code, _signal) {
 
     if (_code != 0) {
-      _req.flash('error', 'Failed to add public key(s): internal error');
-      return _callback(false);
+      return request_error(
+        'Failed to add public key(s): internal error',
+          _req, _callback
+      );
     }
 
-    return _callback(true);
+    return _callback();
   });
 
 };
 
+
 /**
+ * save_system_password:
  */
-var store_system_password = function (_req, _password, _callback) {
+var save_system_password = function (_req, _password, _callback) {
 
   var path = '/srv/storage/concierge/passwd/system';
 
@@ -144,7 +191,7 @@ var store_system_password = function (_req, _password, _callback) {
 
     if (_err) {
       _req.flash('error', "Internal error: file open failed");
-      return _callback(false, _err);
+      return _callback(_err);
     }
 
     var buffer = _password + '\n';
@@ -154,79 +201,153 @@ var store_system_password = function (_req, _password, _callback) {
 
       if (_err) {
         _req.flash('error', "Internal error: file write failed");
-        return _callback(false, _err);
+        return _callback(_err);
       }
 
       fs.fsync(_fd, function (_err) {
-        return _callback(true, _err);
+          return _callback(_err);
       });
     });
   });
 }
 
+
 /**
+ * set_unix_password:
  */
-var set_system_password = function (_req, _password, _confirmation, _callback) {
+var set_unix_password = function (_req, _password, _confirm, _callback) {
 
-  if ((_password || '').length < 8) {
-    _req.flash('error', 'Password must be at least eight characters');
-    return _callback(false);
-  }
-
-  if (_password !== _confirmation) {
-    _req.flash('error', 'Passwords do not match; please try again');
-    return _callback(false);
-  }
-
-  /* Set system password:
-       This can be used to log in via OpenSSH. */
+  /* Set UNIX system password:
+      This can be used to log in via OpenSSH. */
 
   var passwd = child.spawn(
     'sudo', [ 'passwd', 'tc' ], { stdio: 'pipe' }
   );
 
-  passwd.stdin.write(
-    _password + '\n' + _confirmation + '\n'
-  );
-  
+  passwd.stdin.write(_password + '\n' + _confirm + '\n');
+   
   passwd.on('exit', function (_code, _signal) {
 
     if (_code != 0) {
-      _req.flash(
-        'error', 'Password change failed: an internal error occurred'
+      return request_error(
+        'Password change failed: an internal error occurred',
+          _req, _callback
       );
-      return _callback(false);
     }
 
-    /* Set CouchDB administrative password:
-         This takes the database out of 'admin party' mode. */
-
-    var put = {
-      body: JSON.stringify(_password),
-      headers: { 'Content-type': 'application/json' },
-      uri: 'http://localhost:5984/_config/admins/admin'
-    };
-
-    request.put(put, function (err, resp, body) {
-
-      if (resp.statusCode !== 200) {
-        _req.flash(
-          'error', "Password change failed: problem with database server"
-	);
-        return _callback(false, err);
-      }
-
-      /* Store password:
-          This is used by local services needing to connect to CouchDB. */
-
-      store_system_password(_req, _password, function (_was_written, _err) {
-        return _callback(_was_written, _err);
-      });
-    });
+    return _callback();
   });
 };
 
 /**
+ * set_couchdb_password:
+ */
+var set_couchdb_password = function (_req, _password, _confirm, _callback) {
+
+  /* Set CouchDB administrative password:
+      This takes the database out of 'admin party' mode. */
+
+  var proto = 'http://';
+  var server = 'localhost:5984';
+  var admins_uri = server + '/_config/admins'
+
+  var put = {
+    body: JSON.stringify(_password),
+    headers: { 'Content-type': 'application/json' },
+    uri: proto + admins_uri + '/admin'
+  };
+
+  /* Start talking to CouchDB:
+      This process involves multiple requests; async used for clarity. */
+
+  async.waterfall([
+
+    /* Step 1: Primary password change */
+    function (_cb) {
+      request.put(put, function (_err, _resp, _body) {
+	return check_response(
+	  _err, _resp, _req, 'Primary password change', _cb
+	);
+      });
+    },
+
+    /* Step 2: Secondary password generation */
+    function (_cb) {
+      crypto.randomBytes(128, function (_err, _data) {
+	if (_err) {
+	  return _cb(_err);
+	}
+	return _cb(null, _data.toString('hex'));
+      });
+    },
+
+    /* Step 3: Secondary password change */
+    function (_system_password, _cb) {
+
+      put.auth = 'admin:' + _password;
+      put.uri = proto + admins_uri + '/service';
+      put.body = JSON.stringify(_system_password);
+
+      request.put(put, function (_err, _resp, _body) {
+	return check_response(
+	  _err, _resp, _req, 'System account creation', function (_e) {
+	    return _cb(_e, _system_password);
+	  }
+	);
+      });
+    },
+
+    /* Step 4: Filesystem storage of secondary password */
+    function (_system_password, _cb) {
+
+      /* Store secondary administrative password in filesystem:
+	  This is used by local services needing to connect to CouchDB. */
+
+      save_system_password(_req, _system_password, function (_err) {
+	return _cb(_err);
+      });
+    }
+
+  ], function (_err) {
+    return _callback(_err);
+  });
+};      
+
+/**
+ * set_password:
+ */
+var set_password = function (_req, _password, _confirm, _callback) {
+
+  /* Start of password change:
+      Validate the supplied password and confirmation. */
+
+  if ((_password || '').length < 8) {
+    return request_error(
+      'Password must be at least eight characters',
+        _req, _callback
+    );
+  }
+
+  if (_password !== _confirm) {
+    return request_error(
+      'Passwords do not match; please try again',
+        _req, _callback
+    );
+  }
+
+  /* Password databases are modified here */
+  set_unix_password(_req, _password, _confirm, function (_err) {
+
+    if (_err) {
+      return _callback(_err);
+    }
+
+    set_couchdb_password(_req, _password, _confirm, _callback);
+  });
+};
+
+/**
+ * main:
  */
 var main = function (_argv) {
 
