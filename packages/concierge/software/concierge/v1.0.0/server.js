c88b3a1319a28eac5076120f7851613470e8d821
David Brown <dave@scri.pt>
2013-02-02 13:51:01 -0800

    Concierge: make password change repeatable.

--- packages/concierge/software/concierge/v1.0.0/server.js
+++ packages/concierge/software/concierge/v1.0.0/server.js
@@ -8,6 +8,8 @@ var child = require('child_process'),
     fs = require('fs'),
     app = express();
 
+var system_passwd_path = '/srv/storage/concierge/passwd/system';
+
 /**
  */
 app.configure(function () {
@@ -24,14 +26,12 @@ app.configure(function () {
   }));
 });
 
-
 /**
  */
 app.get('/', function (req, res) {
 
   res.redirect('/setup');
 });
-
 
 /**
  */
@@ -183,18 +183,16 @@ var add_openssh_public_key = function (_req, _key, _callback) {
 /**
  * save_system_password:
  */
-var save_system_password = function (_req, _password, _callback) {
+var save_system_password = function (_req, _passwd, _callback) {
 
-  var path = '/srv/storage/concierge/passwd/system';
-
-  fs.open(path, 'w', 0640, function (_err, _fd) {
+  fs.open(system_passwd_path, 'w', 0640, function (_err, _fd) {
 
     if (_err) {
       _req.flash('error', "Internal error: file open failed");
       return _callback(_err);
     }
 
-    var buffer = _password + '\n';
+    var buffer = _passwd + '\n';
     var len = buffer.length;
 
     fs.write(_fd, buffer, 0, len, function (_err, _len, _buf) {
@@ -211,22 +209,41 @@ var save_system_password = function (_req, _password, _callback) {
   });
 }
 
+/**
+ * read_system_password:
+ */
+var read_system_password = function (_callback) {
+
+  try {
+    fs.readFile(system_passwd_path, function (_err, _data) {
+  
+      if (_err) {
+        _req.flash('error', "Internal error: file read failed");
+        return _callback(_err);
+      }
+    
+      return _callback(null, trim(_data.toString()));
+    });
+  } catch (e) {
+    return _callback(e);
+  }
+};
 
 /**
  * set_unix_password:
  */
-var set_unix_password = function (_req, _password, _confirm, _callback) {
+var set_unix_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Set UNIX system password:
       This can be used to log in via OpenSSH. */
 
-  var passwd = child.spawn(
+  var passwd_process = child.spawn(
     'sudo', [ 'passwd', 'tc' ], { stdio: 'pipe' }
   );
 
-  passwd.stdin.write(_password + '\n' + _confirm + '\n');
+  passwd_process.stdin.write(_passwd + '\n' + _confirm + '\n');
    
-  passwd.on('exit', function (_code, _signal) {
+  passwd_process.on('exit', function (_code, _signal) {
 
     if (_code != 0) {
       return request_error(
@@ -242,7 +259,7 @@ var set_unix_password = function (_req, _password, _confirm, _callback) {
 /**
  * set_couchdb_password:
  */
-var set_couchdb_password = function (_req, _password, _confirm, _callback) {
+var set_couchdb_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Set CouchDB administrative password:
       This takes the database out of 'admin party' mode. */
@@ -252,7 +269,7 @@ var set_couchdb_password = function (_req, _password, _confirm, _callback) {
   var admins_uri = server + '/_config/admins'
 
   var put = {
-    body: JSON.stringify(_password),
+    body: JSON.stringify(_passwd),
     headers: { 'Content-type': 'application/json' },
     uri: proto + admins_uri + '/admin'
   };
@@ -262,6 +279,16 @@ var set_couchdb_password = function (_req, _password, _confirm, _callback) {
 
   async.waterfall([
 
+    /* Step 0: Read existing password, if any */
+    function (_cb) {
+      read_system_password(function (_err, _system_passwd) {
+        if (!_err) {
+          put.auth = 'service:' + _system_passwd;
+        }
+        return _cb();
+      });
+    },
+    
     /* Step 1: Primary password change */
     function (_cb) {
       request.put(put, function (_err, _resp, _body) {
@@ -282,28 +309,28 @@ var set_couchdb_password = function (_req, _password, _confirm, _callback) {
     },
 
     /* Step 3: Secondary password change */
-    function (_system_password, _cb) {
+    function (_system_passwd, _cb) {
 
-      put.auth = 'admin:' + _password;
+      put.auth = 'admin:' + _passwd;
       put.uri = proto + admins_uri + '/service';
-      put.body = JSON.stringify(_system_password);
+      put.body = JSON.stringify(_system_passwd);
 
       request.put(put, function (_err, _resp, _body) {
 	return check_response(
 	  _err, _resp, _req, 'System account creation', function (_e) {
-	    return _cb(_e, _system_password);
+	    return _cb(_e, _system_passwd);
 	  }
 	);
       });
     },
 
     /* Step 4: Filesystem storage of secondary password */
-    function (_system_password, _cb) {
+    function (_system_passwd, _cb) {
 
       /* Store secondary administrative password in filesystem:
 	  This is used by local services needing to connect to CouchDB. */
 
-      save_system_password(_req, _system_password, function (_err) {
+      save_system_password(_req, _system_passwd, function (_err) {
 	return _cb(_err);
       });
     }
@@ -316,19 +343,19 @@ var set_couchdb_password = function (_req, _password, _confirm, _callback) {
 /**
  * set_password:
  */
-var set_password = function (_req, _password, _confirm, _callback) {
+var set_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Start of password change:
       Validate the supplied password and confirmation. */
 
-  if ((_password || '').length < 8) {
+  if ((_passwd || '').length < 8) {
     return request_error(
       'Password must be at least eight characters',
         _req, _callback
     );
   }
 
-  if (_password !== _confirm) {
+  if (_passwd !== _confirm) {
     return request_error(
       'Passwords do not match; please try again',
         _req, _callback
@@ -336,13 +363,13 @@ var set_password = function (_req, _password, _confirm, _callback) {
   }
 
   /* Password databases are modified here */
-  set_unix_password(_req, _password, _confirm, function (_err) {
+  set_unix_password(_req, _passwd, _confirm, function (_err) {
 
     if (_err) {
       return _callback(_err);
     }
 
-    set_couchdb_password(_req, _password, _confirm, _callback);
+    set_couchdb_passwd(_req, _passwd, _confirm, _callback);
   });
 };
 
