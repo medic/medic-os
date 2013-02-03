3523e2190cf5e1a33d497832efeb7632087ef184
David Brown <dave@scri.pt>
2013-02-02 18:14:53 -0800

    Merge from branch 'dev'.

--- packages/concierge/software/concierge/v1.0.0/server.js
+++ packages/concierge/software/concierge/v1.0.0/server.js
@@ -8,6 +8,10 @@ var child = require('child_process'),
     fs = require('fs'),
     app = express();
 
+var protocol = 'http://';
+var server = 'localhost:5984';
+var system_passwd_path = '/srv/storage/concierge/passwd/system';
+
 /**
  */
 app.configure(function () {
@@ -24,14 +28,12 @@ app.configure(function () {
   }));
 });
 
-
 /**
  */
 app.get('/', function (req, res) {
 
   res.redirect('/setup');
 });
-
 
 /**
  */
@@ -65,25 +67,32 @@ app.post('/setup/password', function (req, res) {
     req.flash('key', key);
   }
 
-  set_password(req, password, confirmation, function (_err) {
-
+  set_password(req, password, confirmation, function (_err, _system_passwd) {
+  
     if (_err) {
       return res.redirect('/setup');
     }
 
-    if (key.length <= 0) {
-      req.flash('success', 'Password successfully set');
-      return res.redirect('/setup');
-    }
+    add_couchdb_defaults(req, _system_passwd, function (_err) {
 
-    add_openssh_public_key(req, key, function (_was_added) {
-
-      if (_was_added) {
-        req.flash('key', null);
-        req.flash('success', 'Password and public key successfully set');
+      if (_err) {
+        return res.redirect('/setup');
+      }
+      
+      if (key.length <= 0) {
+        req.flash('success', 'Password successfully set');
+        return res.redirect('/setup');
       }
 
-      return res.redirect('/setup');
+      add_openssh_public_key(req, key, function (_err) {
+
+        if (!_err) {
+          req.flash('key', null);
+          req.flash('success', 'Password and public key successfully set');
+        }
+        
+        return res.redirect('/setup');
+      });
     });
 
   });
@@ -183,18 +192,16 @@ var add_openssh_public_key = function (_req, _key, _callback) {
 /**
  * save_system_password:
  */
-var save_system_password = function (_req, _password, _callback) {
+var save_system_password = function (_req, _passwd, _callback) {
 
-  var path = '/srv/storage/concierge/passwd/system';
-
-  fs.open(path, 'w', 0640, function (_err, _fd) {
+  fs.open(system_passwd_path, 'w', 0640, function (_err, _fd) {
 
     if (_err) {
       _req.flash('error', "Internal error: file open failed");
       return _callback(_err);
     }
 
-    var buffer = _password + '\n';
+    var buffer = _passwd + '\n';
     var len = buffer.length;
 
     fs.write(_fd, buffer, 0, len, function (_err, _len, _buf) {
@@ -205,28 +212,46 @@ var save_system_password = function (_req, _password, _callback) {
       }
 
       fs.fsync(_fd, function (_err) {
-          return _callback(_err);
+        return _callback(_err);
       });
     });
   });
 }
 
+/**
+ * read_system_password:
+ */
+var read_system_password = function (_callback) {
+
+  try {
+    fs.readFile(system_passwd_path, function (_err, _data) {
+  
+      if (_err) {
+        return _callback(_err);
+      }
+    
+      return _callback(null, trim(_data.toString()));
+    });
+  } catch (e) {
+    return _callback(e);
+  }
+};
 
 /**
  * set_unix_password:
  */
-var set_unix_password = function (_req, _password, _confirm, _callback) {
+var set_unix_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Set UNIX system password:
       This can be used to log in via OpenSSH. */
 
-  var passwd = child.spawn(
+  var passwd_process = child.spawn(
     'sudo', [ 'passwd', 'tc' ], { stdio: 'pipe' }
   );
 
-  passwd.stdin.write(_password + '\n' + _confirm + '\n');
+  passwd_process.stdin.write(_passwd + '\n' + _confirm + '\n');
    
-  passwd.on('exit', function (_code, _signal) {
+  passwd_process.on('exit', function (_code, _signal) {
 
     if (_code != 0) {
       return request_error(
@@ -242,19 +267,17 @@ var set_unix_password = function (_req, _password, _confirm, _callback) {
 /**
  * set_couchdb_password:
  */
-var set_couchdb_password = function (_req, _password, _confirm, _callback) {
+var set_couchdb_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Set CouchDB administrative password:
       This takes the database out of 'admin party' mode. */
 
-  var proto = 'http://';
-  var server = 'localhost:5984';
   var admins_uri = server + '/_config/admins'
 
   var put = {
-    body: JSON.stringify(_password),
-    headers: { 'Content-type': 'application/json' },
-    uri: proto + admins_uri + '/admin'
+    body: JSON.stringify(_passwd),
+    uri: protocol + admins_uri + '/admin',
+    headers: { 'Content-type': 'application/json' }
   };
 
   /* Start talking to CouchDB:
@@ -262,73 +285,94 @@ var set_couchdb_password = function (_req, _password, _confirm, _callback) {
 
   async.waterfall([
 
-    /* Step 1: Primary password change */
+    /* Step 0: Read system password, if it's already set */
     function (_cb) {
-      request.put(put, function (_err, _resp, _body) {
-	return check_response(
-	  _err, _resp, _req, 'Primary password change', _cb
-	);
+      read_system_password(function (_err, _system_passwd) {
+        if (!_err) {
+          put.auth = 'service:' + _system_passwd;
+        }
+        /* Ignore errors: file might not exist */
+        return _cb(null, _system_passwd);
       });
     },
-
-    /* Step 2: Secondary password generation */
-    function (_cb) {
-      crypto.randomBytes(128, function (_err, _data) {
-	if (_err) {
-	  return _cb(_err);
-	}
-	return _cb(null, _data.toString('hex'));
-      });
-    },
-
-    /* Step 3: Secondary password change */
-    function (_system_password, _cb) {
-
-      put.auth = 'admin:' + _password;
-      put.uri = proto + admins_uri + '/service';
-      put.body = JSON.stringify(_system_password);
-
+    
+    /* Step 1: Primary password change (i.e. admin) */
+    function (_system_passwd, _cb) {
       request.put(put, function (_err, _resp, _body) {
 	return check_response(
-	  _err, _resp, _req, 'System account creation', function (_e) {
-	    return _cb(_e, _system_password);
+	  _err, _resp, _req, 'Primary password change', function(_e) {
+	    return _cb(_e, _system_passwd);
 	  }
 	);
       });
     },
 
-    /* Step 4: Filesystem storage of secondary password */
-    function (_system_password, _cb) {
+    /* Step 2: Secondary password generation, if necessary */
+    function (_system_passwd, _cb) {
+      if (_system_passwd) {
+        return _cb(null, _system_passwd, false);
+      }
+      crypto.randomBytes(128, function (_err, _data) {
+	if (_err) {
+	  return _cb(_err);
+	}
+	return _cb(null, _data.toString('hex'), true);
+      });
+    },
+
+    /* Step 3: Secondary password change */
+    function (_system_passwd, _first_run, _cb) {
+    
+      if (_first_run) {
+        put.auth = 'admin:' + _passwd;
+      } else {
+        put.auth = 'service:' + _system_passwd;
+      }
+        
+      put.body = JSON.stringify(_system_passwd);
+      put.uri = protocol + admins_uri + '/service';
+
+      request.put(put, function (_err, _resp, _body) {
+	return check_response(
+	  _err, _resp, _req, 'System account creation', function (_e) {
+	    return _cb(_e, _system_passwd);
+	  }
+	);
+      });
+    },
+
+    /* Step 4: In-filesystem storage of secondary password */
+    function (_system_passwd, _cb) {
 
       /* Store secondary administrative password in filesystem:
 	  This is used by local services needing to connect to CouchDB. */
 
-      save_system_password(_req, _system_password, function (_err) {
-	return _cb(_err);
+      save_system_password(_req, _system_passwd, function (_err) {
+	return _cb(_err, _system_passwd);
       });
     }
 
-  ], function (_err) {
-    return _callback(_err);
+  ], function (_err, _system_passwd) {
+    return _callback(_err, _system_passwd);
   });
 };      
 
 /**
  * set_password:
  */
-var set_password = function (_req, _password, _confirm, _callback) {
+var set_password = function (_req, _passwd, _confirm, _callback) {
 
   /* Start of password change:
       Validate the supplied password and confirmation. */
 
-  if ((_password || '').length < 8) {
+  if ((_passwd || '').length < 8) {
     return request_error(
       'Password must be at least eight characters',
         _req, _callback
     );
   }
 
-  if (_password !== _confirm) {
+  if (_passwd !== _confirm) {
     return request_error(
       'Passwords do not match; please try again',
         _req, _callback
@@ -336,15 +380,46 @@ var set_password = function (_req, _password, _confirm, _callback) {
   }
 
   /* Password databases are modified here */
-  set_unix_password(_req, _password, _confirm, function (_err) {
+  set_unix_password(_req, _passwd, _confirm, function (_err) {
 
     if (_err) {
       return _callback(_err);
     }
 
-    set_couchdb_password(_req, _password, _confirm, _callback);
+    set_couchdb_password(_req, _passwd, _confirm, _callback);
   });
 };
+
+/**
+ * add_couchdb_defaults:
+ */
+var add_couchdb_defaults = function (_req, _system_passwd, _callback) {
+
+  var put = {
+    auth: 'service:' + _system_passwd,
+    headers: { 'Content-Type': 'application/json' }
+  };
+  
+  async.waterfall([
+
+    /* Step 1: Restrict CouchDB to valid users */
+    function (_cb) {
+      var config_uri = server + '/_config/couch_httpd_auth'
+
+      put.body = '"true"';
+      put.uri = protocol + config_uri + '/require_valid_user';
+
+      request.put(put, function (_err, _resp, _body) {
+	return check_response(
+	  _err, _resp, _req, 'Configuration change', _cb
+	);
+      });
+    }
+  ], 
+  function (_err) {
+    return _callback(_err);
+  });
+}
 
 /**
  * main:
