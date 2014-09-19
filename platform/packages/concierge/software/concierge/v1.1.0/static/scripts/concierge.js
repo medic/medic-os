346e404315dc30d8bec413559b1abfd0dff11e5a
David Brown <dave@scri.pt>
2014-09-18 17:16:03 -0700

    Remove old CSS/JS, add client-side service polling code.
    This strips the concierge user interface down to an unformatted bare minimum,
    and adds a client-side `wait_for_required_services` function that uses the
    `/setup/poll` REST interface.

--- platform/packages/concierge/software/concierge/v1.1.0/static/scripts/concierge.js
+++ platform/packages/concierge/software/concierge/v1.1.0/static/scripts/concierge.js
@@ -1,93 +1,71 @@
 
-var resize_handler = function () {
+/**
+ * poll_required_services:
+ */
+var poll_required_services = function (_url, _callback) {
 
-  var top_elt = $('.login-top');
-  var top_height = top_elt.height();
-  var background_elt = $('.login-background');
-
-  $('.login-bottom').css({
-    top: (top_elt.position().top + top_height) + 'px',
-    height: Math.max(background_elt.height() - top_height, 0) + 'px'
-  });
-};
-
-var textarea_focus = function (_elts) {
-
-  $(_elts).each(function (_i, _elt) {
-
-    var elt = $(_elt);
-    var wrap_elt = elt.parents('.textarea-wrapper').first();
-
-    elt.focus(function () {
-      wrap_elt.addClass('textarea-focus');
-    });
-
-    elt.blur(function () {
-      wrap_elt.removeClass('textarea-focus');
-    });
-  });
-};
-
-var button_hover = function (_elts) {
-
-  $(_elts).each(function (_i, _elt) {
-
-    var elt = $(_elt);
-    
-    if (elt.hasClass('disabled')) {
-      return;
+  $.ajax(_url, {
+    success: function (_data, _status, _xhr) {
+      return _callback(null, _data);
+    },
+    error: function (_xhr, _status, _err) {
+      return _callback(_err);
     }
-      
-    elt.mouseenter(function () {
-      elt.addClass('hover');
-    });
-
-    elt.mouseleave(function () {
-      elt.removeClass('hover');
-    });
   });
 };
 
-var button_submit = function (_submit_elt, _elts) {
+/**
+ * wait_for_required_services:
+ */
+var wait_for_required_services = function (_url, _options, _callback) {
 
-  var submit_elt = $(_submit_elt);
-  var form_elt = submit_elt.parents('form').first();
-  
-  var toggle_image = function (_elt) {
-    $(_elt).find('.icon-lock-open')
-      .removeClass('icon-lock-open').addClass('icon-lock-closed');
-  };
+  var options = (_options || {});
+  var limit = (options.limit || 30);
+  var interval = (options.interval || 2000);
 
-  $(_elts).each(function (_i, _elt) {
+  poll_required_services(_url, function (_err, _result) {
 
-    var elt = $(_elt);
+    if (_err) {
+      return _cb(_err);   /* Network error */
+    }
 
-    elt.click(function () {
-      toggle_image(elt);
+    if (_result.ready) {
+
+      /* Success */
+      return _callback(null, _result);
+
+    } else {
+
+      /* Check iteration limit */
+      if (--limit <= 0) {
+        return _callback(null, {
+          ready: false,
+          detail: 'Timed out while waiting for required services'
+        });
+      }
+
+      /* Iteration complete */
+      options.limit = limit;
       
-      setTimeout(function () {  
-        submit_elt.click();
-      }, 100);
-      
-    });
+      /* Reschedule */
+      setTimeout(function () {
+        wait_for_required_services(_url, options, _callback);
+      }, interval);
+    }
   });
 };
 
+/**
+ * main:
+ */
 jQuery(function ($) {
 
-  resize_handler();
-  button_hover('#save');
-  button_hover('#lock');
-  textarea_focus('#key');
-  button_submit('#submit', '#save');
-  $(window).resize(resize_handler);
-
   $('#lock').click(function (_ev) {
-  
+
     if ($(this).hasClass('disabled')) {
       return false;
     }
-    
+
     $.ajax({
       url: 'setup/finish',
       complete: function (_xhr, _status) {
@@ -95,7 +73,6 @@ jQuery(function ($) {
       }
     });
   });
-  
 });
 
 
